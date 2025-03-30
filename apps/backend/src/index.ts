cb60065f767444107c96c7e53873f3def6e6d4e7
Anirban Singha <anirbansingha20@gmail.com>
2025-03-30 18:23:51 +0530

    Configure packages and setup basic structure

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,58 @@
 import express from "express"
+import { JWT_SECRET } from "@repo/backend-common/config"
+import jwt from "jsonwebtoken"
+import { middleware } from "./middleware"
+import {CreateUserSchema} from "@repo/common/types"
 
 const app = express()
+
+app.post('/signup', async(req, res) => {
+    const data = CreateUserSchema.safeParse(req.body)
+
+    if(!data.success){
+        res.json({
+            message: "Incorret inputs"
+        })
+
+        return
+    }
+
+})
+
+app.post('/signin', (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body)
+
+    if(!data.success){
+        res.json({
+            message: "Incorret inputs"
+        })
+
+        return
+    }
+
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId, 
+    },
+    JWT_SECRET)
+
+    res.json({
+        token: token
+    })
+})
+
+app.post('/room', middleware, (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body)
+
+    if(!data.success){
+        res.json({
+            message: "Incorret inputs"
+        })
+
+        return
+    }
+})
 
 app.listen(3000)
