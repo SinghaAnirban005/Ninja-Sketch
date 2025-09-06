a3f21e33991726a2ca6e831d6707ad606716d0d8
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-09-06 13:33:40 +0530

    whitelist client endpoint and urls
    Signed-off-by: SinghaAnirban005 <anirbansingha20@gmail.com>

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:4000"],
+    origin: ["http://localhost:3000", "http://localhost:4000", "https://ninja-sketch-8vy9.onrender.com"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
--- apps/ninja-frontend/config.ts
+++ apps/ninja-frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_URL = "http://localhost:4000";
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_URL = "https://ninja-sketch-2.onrender.com";
+export const WS_URL = "https://ninja-sketch-ws.onrender.com";
